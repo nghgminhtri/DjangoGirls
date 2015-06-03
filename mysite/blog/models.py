34bd6f4496e67e3c3c35c5f962ca3d29f0a6f873
Tri Nguyen <tri.nguyen@dirox.net>
2015-06-03 17:29:17 +0700

    Added delete, unpublish

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,6 +13,10 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+        
+    def unpublish(self):
+        self.published_date = None
+        self.save()
 
     def __str__(self):
         return self.title
